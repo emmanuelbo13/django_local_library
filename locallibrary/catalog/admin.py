829d9c6aed2e6d830f41321718169a7f524be55a
cyber_e <bustemma90@gmail.com>
2025-12-18 23:23:42 -0500

    Add base and index template, custom queries for displaying information

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
+# Inline class for BookInstance; to be displayed in Book admin page
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance 
-    classes = ('collapse',)
+    classes = ('collapse',) # to make it collapsible in the admin interface
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -11,14 +12,15 @@ class BookAdmin(admin.ModelAdmin):
 
     inlines = [BooksInstanceInline]
 
+# Inline class for Book; to be displayed in Author admin page
 class BookInline(admin.TabularInline):
     model = Book
     classes = ('collapse',)
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death') # display columns in admin list view
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')] # layout of fields in admin detail view
 
     inlines = [BookInline]
 
@@ -26,10 +28,10 @@ admin.site.register(Genre)
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_filter = ('status', 'due_back')
+    list_filter = ('status', 'due_back') # filters in the right sidebar
     list_display = ('display_book', 'status', 'due_back', 'id')
 
-    # display by sections
+    # organize detail view layout
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
@@ -38,6 +40,5 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('status', 'due_back')
         }),
     )
-
 
 admin.site.register(Language)
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+def index(request):
+    """View function for home page of site"""
+    num_books = Book.objects.all().count()
+    num_instaces = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instaces_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # genre count
+    num_genres = Genre.objects.all().count()
+
+    # Filtering books by a specific genre
+    fantasy_books = Book.objects.filter(genre__name__icontains='fantasy')
+    fantasy_books_count = fantasy_books.count()
+
+    # The 'all()' is implied by default
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'fantasy_books_count': fantasy_books_count,
+        'num_instances': num_instaces, 
+        'num_instances_available': num_instaces_available, 
+        'num_authors': num_authors,
+        'fantasy_books': fantasy_books, 
+        'num_genres': num_genres
+    }
+
+    print(type(fantasy_books))
+    return render(request, 'index.html', context=context)   
